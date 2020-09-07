2a20f914dfb14d56cba02473ad84f340d4f36f5e
sultan nadim <sultan5218@gmail.com>
2020-09-07 22:36:57 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//product details image start
+
+
+var ProductImg = document.getElementById("ProductImg");
+var SmallProduct = document.getElementsByClassName("small_product");
+
+SmallProduct[0].onclick = function(){
+    
+    ProductImg.src = SmallProduct[0].src
+}
+
+SmallProduct[1].onclick = function(){
+    
+    ProductImg.src = SmallProduct[1].src
+}
+
+SmallProduct[2].onclick = function(){
+    
+    ProductImg.src = SmallProduct[2].src
+}
+
+SmallProduct[3].onclick = function(){
+    
+    ProductImg.src = SmallProduct[3].src
+}
+
+//product detail image finish
+
+
+//account page start
+
+
+
+
+
+
+
+//account page finish
+
+
+
+
+
+
